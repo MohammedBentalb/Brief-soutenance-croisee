fc8e4e43247e5494b0f71b83269e573e9e36db54
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2025-11-19 11:13:59 +0100

    feat: bbuilding the modal's structure using html and css

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -41,14 +41,14 @@ export const showError = (errorField: HTMLParagraphElement, mssg: string) => {
 export function validatingDates(dateInp1: HTMLDataElement, dateInp2: HTMLDataElement): string[] {
   let date1Error = "", date2Error = "";
 
-  if(!dateInp1.value.trim() && !dateInp2.value.trim()) return ["you must set a start date", "you must set an end date"]
-  if(dateInp1.value.trim() && !dateInp2.value.trim()) return ["", "end date must be set too"]
-  if(!dateInp1.value.trim() && dateInp2.value.trim()) date1Error = "first date must be set before end date" 
-  
+  if (!dateInp1.value.trim() && !dateInp2.value.trim()) return ["you must set a start date", "you must set an end date"]
+  if (dateInp1.value.trim() && !dateInp2.value.trim()) return ["", "end date must be set too"]
+  if (!dateInp1.value.trim() && dateInp2.value.trim()) date1Error = "first date must be set before end date"
+
   const date = new Date();
   const [todayYear, todayMonth, todayDay] = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
   const [year, month, day] = dateInp1.value.split("-").map((d) => Number(d));
-  
+
 
   if (year > todayYear || (todayYear === year && month > todayMonth) || (todayYear === year && month === todayMonth && day > todayDay)) {
     date1Error = "Start date can't be ahead or equal to today";
@@ -66,35 +66,26 @@ export function validatingDates(dateInp1: HTMLDataElement, dateInp2: HTMLDataEle
 }
 
 export function AddToExperienceErrorArray(
-  word: string,
-  input: validatingInputValueType,
-  errorArray: string[] | []
-) {
+  word: string, input: validatingInputValueType, errorArray: string[] | []) {
   let newArr = [...errorArray];
 
   if (word) {
-    newArr = newArr.includes(input.id as never)
-      ? newArr
-      : [...newArr, input.id];
-  } else {
-    newArr = newArr.includes(input.id as never)
-      ? newArr.filter((s) => s !== input.id)
-      : newArr;
-  }
-  return newArr;
+    return newArr = newArr.includes(input.id as never) ? newArr : [...newArr, input.id]
+  } 
+  return newArr = newArr.includes(input.id as never) ? newArr.filter((s) => s !== input.id) : newArr;
 }
 
-export function handleExperienceShowinfErrorAndValidation(input: validatingInputValueType, errorField: HTMLParagraphElement, ErrorArr: string[] | []){
+export function handleExperienceShowinfErrorAndValidation(input: validatingInputValueType, errorField: HTMLParagraphElement, ErrorArr: string[] | []) {
   let newErrorArray = [...ErrorArr];
 
   const validationResult = validatingInputValue(input);
-  newErrorArray = AddToExperienceErrorArray( validationResult, input, newErrorArray);
+  newErrorArray = AddToExperienceErrorArray(validationResult, input, newErrorArray);
   showError(errorField, validationResult);
 
   return { valid: validationResult !== "", errorArray: newErrorArray };
 }
 
-export function handleInputShowinfErrorAndValidation( input: validatingInputValueType, errorField: HTMLParagraphElement){
+export function handleInputShowinfErrorAndValidation(input: validatingInputValueType, errorField: HTMLParagraphElement) {
   const validationResult = validatingInputValue(input);
   showError(errorField, validationResult);
 
